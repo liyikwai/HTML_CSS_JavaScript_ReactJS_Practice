2413d1af0d9ecf7e020c3f8a1dc63ab555c3ae0e
LI YIK WAI <u3556601@connect.hku.hk>
2020-06-01 12:05:25 +0800

    Show

---
+++ Homework(Static Webpage)/js/main.js
@@ -0,0 +1,208 @@
+var jsonData = {
+  data: {
+    0: {
+      theme: "新闻安大",
+      content: [
+        {
+          "title": "十三届全国人大三次会议开幕会举行 习近平等党和国家领导人出席",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": "省委第二巡视组巡视安徽大学党委工作动员会召开",
+          "publisheddate": "05-12",
+          "url": "http://m.ahjjjc.gov.cn/p/84443.html"
+        },
+        {
+          "title": "【聚焦“双一流”】我校与南陵县人民政府签署合作框架协议",
+          "publisheddate": "05-02",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": "【聚焦“双一流”】我校召开2020年毕业生就业工作大会",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": "我校师生热议2020年全国“两会”",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": " 【脱贫攻坚】安徽大学国家语言文字推广基地对口甘肃省镇原县教师国家通用语言文字能力线上培训正式开班",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+      ]
+    },
+    1: {
+      theme: "学术安大",
+      content: [
+        {
+          "title": "十三届全国人大三次会议开幕会举行 习近平等党和国家领导人出席",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": "省委第二巡视组巡视安徽大学党委工作动员会召开",
+          "publisheddate": "05-12",
+          "url": "http://m.ahjjjc.gov.cn/p/84443.html"
+        },
+        {
+          "title": "【聚焦“双一流”】我校与南陵县人民政府签署合作框架协议",
+          "publisheddate": "05-02",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": "【聚焦“双一流”】我校召开2020年毕业生就业工作大会",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": "我校师生热议2020年全国“两会”",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": " 【脱贫攻坚】安徽大学国家语言文字推广基地对口甘肃省镇原县教师国家通用语言文字能力线上培训正式开班",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+      ]
+    },
+    2: {
+      theme: "信息安大",
+      content: [
+        {
+          "title": "十三届全国人大三次会议开幕会举行 习近平等党和国家领导人出席",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": "省委第二巡视组巡视安徽大学党委工作动员会召开",
+          "publisheddate": "05-12",
+          "url": "http://m.ahjjjc.gov.cn/p/84443.html"
+        },
+        {
+          "title": "【聚焦“双一流”】我校与南陵县人民政府签署合作框架协议",
+          "publisheddate": "05-02",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": "【聚焦“双一流”】我校召开2020年毕业生就业工作大会",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": "我校师生热议2020年全国“两会”",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+      ]
+    },
+    3: {
+      theme: "媒体安大",
+      content: [
+        {
+          "title": "十三届全国人大三次会议开幕会举行 习近平等党和国家领导人出席",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": "省委第二巡视组巡视安徽大学党委工作动员会召开",
+          "publisheddate": "05-12",
+          "url": "http://m.ahjjjc.gov.cn/p/84443.html"
+        },
+        {
+          "title": "【聚焦“双一流”】我校与南陵县人民政府签署合作框架协议",
+          "publisheddate": "05-02",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": "【聚焦“双一流”】我校召开2020年毕业生就业工作大会",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": "我校师生热议2020年全国“两会”",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": " 【脱贫攻坚】安徽大学国家语言文字推广基地对口甘肃省镇原县教师国家通用语言文字能力线上培训正式开班",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+      ]
+    },
+    4: {
+      theme: "活力安大",
+      content: [
+        {
+          "title": "十三届全国人大三次会议开幕会举行 习近平等党和国家领导人出席",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": "省委第二巡视组巡视安徽大学党委工作动员会召开",
+          "publisheddate": "05-12",
+          "url": "http://m.ahjjjc.gov.cn/p/84443.html"
+        },
+        {
+          "title": "【聚焦“双一流”】我校与南陵县人民政府签署合作框架协议",
+          "publisheddate": "05-02",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": "【聚焦“双一流”】我校召开2020年毕业生就业工作大会",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": "我校师生热议2020年全国“两会”",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": " 【脱贫攻坚】安徽大学国家语言文字推广基地对口甘肃省镇原县教师国家通用语言文字能力线上培训正式开班",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+      ]
+    },
+    5: {
+      theme: "校友安大",
+      content: [
+        {
+          "title": "【聚焦“双一流”】我校召开2020年毕业生就业工作大会",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": "我校师生热议2020年全国“两会”",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+        {
+          "title": " 【脱贫攻坚】安徽大学国家语言文字推广基地对口甘肃省镇原县教师国家通用语言文字能力线上培训正式开班",
+          "publisheddate": "05-22",
+          "url": "http://www.xinhuanet.com/politics/2020lh/zb/qh/qh2/index.htm"
+        },
+      ]
+    },
+  }
+}
+
+
+var itemList = $('.list-item-li');
+
+for (var i = 0; i < itemList.length; i++) {
+  itemList.eq(i).children('.item-title').text(jsonData.data[i].theme);
+  for (var j = 0; j < jsonData.data[i].content.length; j++) {
+    itemList.eq(i).children('.sub-list-item-ul').append('<li class="sub-list-item-li"><span>' +
+        jsonData.data[i].content[j].publisheddate +
+        '</span><a href="'+jsonData.data[i].content[j].url+'" title="'+jsonData.data[i].content[j].title+'" target="_blank">' +
+        jsonData.data[i].content[j].title +
+        '</a></li>')
+  }
+}
